92d127d05c5d371a064af0ffbaed62b8abd988a9
ysubtle <bananagus@gmail.com>
2018-05-14 17:46:15 -0600

    Add member tag

--- query/encode.go
+++ query/encode.go
@@ -347,7 +347,7 @@ func reflectValueAmazon(values url.Values, val reflect.Value, scope string) erro
 				for i := 0; i < sv.Len(); i++ {
 					k := name
 					if opts.Contains("numbered") {
-						k = fmt.Sprintf("%s%d", name, i)
+						k = fmt.Sprintf("%s.member.%d.", name, i)
 					}
 					values.Add(k, valueString(sv.Index(i), opts))
 				}
